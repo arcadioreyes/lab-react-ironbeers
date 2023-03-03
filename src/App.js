4a0031fa89bc97aa22f06f8dbd7a9a16c0f385d0
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-03-03 19:27:43 +0100

    axios, bootstrap, bootstrap-icons installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "./components/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import allBeersImg from "../assets/beers.png";
+import randomBeerImg from "../assets/random-beer.png";
+import newBeerImg from "../assets/new-beer.png";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>This is the Homepage component</h1>
+    </div>
+  );
+}
+
+export default Homepage;
